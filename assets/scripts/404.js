73b1cfbf242b16f3d14b946a35e552657626ad6d
Devan <devankav7@gmail.com>
2020-12-11 21:10:31 -0500

    updated 404 page

--- assets/scripts/404.js
+++ assets/scripts/404.js
@@ -8,6 +8,35 @@ function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms))
 }
 
+function pixelfy(number) {
+    /*
+    Converts a number into a string and adds "px" to the end. 
+    Example: 200 -> "200px"
+
+    Parameters:
+        number: The number being converted
+    */
+    return number.toString() + 'px'
+}
+
+function center(tag) {
+    /*
+    Centers a given element.
+
+    Parameters:
+        tag: The name of the element
+    */
+    var width = $(window).width()
+
+    $(tag).css('left', pixelfy((width / 2) - ($(tag).width() / 2)))
+}
+
+function resize() {
+    console.log(1)
+    $('#homeButton').css('top', pixelfy(($(window).height() / 2) + $('#textbox404').height()))
+    center('#homeButton')
+}
+
 async function introAnimation() {
     var text = 'Page Not Found'
     $('#cursor').css('display', 'inline')
@@ -39,4 +68,9 @@ function cursor() {
 
 }
 
-$(document).ready(introAnimation)
+$(document).ready(function() {
+    resize()
+    introAnimation()
+})
+
+$(window).resize(resize)
