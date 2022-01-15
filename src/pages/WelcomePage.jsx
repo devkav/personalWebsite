433cd6faa05cb15e735142a47738505b1c74b9e1
Devan Kavalchek <devankav7@gmail.com>
2022-01-15 15:13:14 -0500

    added gradient following cursor

--- src/pages/WelcomePage.jsx
+++ src/pages/WelcomePage.jsx
@@ -4,20 +4,57 @@ import LearnMore from '../components/LearnMore';
 import Navbar from '../components/Navbar';
 
 class WelcomePage extends Component {
+  constructor(props) {
+    super(props)
+    
+    this.state = {
+      degrees: 0
+    }
+  }
+
+  onMouseMove = (e) => {
+    let centerX = window.innerWidth / 2
+    let centerY = window.innerHeight / 2
+    let currentX = e.screenX
+    let currentY = e.screenY
+    let rise = centerY + (centerY - currentY)
+    let run = currentX - centerX
+
+    let radians = Math.atan(rise / run)
+    let degrees = radians * (180 / Math.PI)
+
+    if (run >= 0) {
+      if (rise <= 0) {
+        degrees += 360
+      }
+    } else {
+        degrees += 180
+    }
+
+    degrees = Math.round(degrees)
+
+    if (this.state.degrees !== degrees) {
+      this.setState({degrees: degrees})
+    }
+  }
   render() {
     return (
-      <div className="welcome-page">
+      <div className="welcome-page" onMouseMove={this.onMouseMove}>
         <Navbar />
 
         <div className="header-col centered-col">
           <Container className="welcome-text-container">
             <h3 className="welcome-text">Hi, my name is</h3>
-            <h1 className="welcome-text">Devan<br />Kavalchek</h1>
+            <h1 className="welcome-text" style={{
+              "backgroundImage": "-webkit-linear-gradient(" + this.state.degrees + "deg, #7E3FF2, #f23fb3)",
+              "backgroundClip": "text",
+              "WebkitTextFillColor": "transparent"
+            }}>Devan<br />Kavalchek</h1>
             <h3 className="welcome-text">A detail oriented software engineer.</h3>
           </Container>
         </div>
 
-        <LearnMore/>
+        <LearnMore />
       </div>
     );
   }
