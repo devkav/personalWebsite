8ea7b0df082f74e9f1ef40c97608c8eb96433572
Devan Kavalchek <devankav7@gmail.com>
2022-01-14 23:32:12 -0500

    added email btn

--- src/components/Linkbar.jsx
+++ src/components/Linkbar.jsx
@@ -12,6 +12,7 @@ class Linkbar extends Component {
     openGithub = () => {this.openLink("https://github.com/DevanTurtle7")}
     openLinkedIn = () => {this.openLink("https://www.linkedin.com/in/devan-kavalchek")}
     openDevpost = () => {this.openLink("https://devpost.com/djk6908")}
+    openEmail = () => {this.openLink("mailto:devan.kavalchek@gmail.com")}
     openResume = () => {this.openLink("./resume.pdf")}
 
     render() {
@@ -20,7 +21,7 @@ class Linkbar extends Component {
                 <AiOutlineGithub className="linkbar-icon color-0" onClick={this.openGithub}/>
                 <FaLinkedinIn className="linkbar-icon color-25" onClick={this.openLinkedIn}/>
                 <SiDevpost className="linkbar-icon color-50" onClick={this.openDevpost}/>
-                <MdEmail className="linkbar-icon color-75" />
+                <MdEmail className="linkbar-icon color-75" onClick={this.openEmail}/>
                 <AiFillFile className="linkbar-icon color-100" onClick={this.openResume}/>
             </div>
         );
