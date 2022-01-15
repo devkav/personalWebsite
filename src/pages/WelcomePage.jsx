4ddf93303ef13368874f9a2539ffdff7c6896a84
Devan Kavalchek <devankav7@gmail.com>
2022-01-14 22:21:50 -0500

    stubbed site

---
+++ src/pages/WelcomePage.jsx
@@ -0,0 +1,13 @@
+import { Component } from 'react';
+import { Col } from 'reactstrap';
+
+class WelcomePage extends Component {
+  render() {
+    return (
+      <Col>
+      </Col>
+    );
+  }
+}
+
+export default WelcomePage;
