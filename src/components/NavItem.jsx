9397a25c85841fae18a703197961bbf768f54eb7
Devan Kavalchek <devankav7@gmail.com>
2022-01-15 19:01:42 -0500

    updated animations

--- src/components/NavItem.jsx
+++ src/components/NavItem.jsx
@@ -1,14 +1,37 @@
 import { Component } from 'react';
 
 class NavItem extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            loaded: false
+        }
+    }
+
+    sleep = (ms) => {
+        return new Promise(resolve => setTimeout(resolve, ms));
+    }
+
+    componentDidMount() {
+        this.load()
+    }
+
+    load = async () => {
+        await this.sleep(1000 + (this.props.order * 300))
+        this.setState({loaded: true})
+    }
+
     onClick = () => {
         let section = document.getElementById(this.props.section)
         section.scrollIntoView({ behavior: 'smooth' })
     }
 
     render() {
+        let className = this.state.loaded ? "nav-item-loaded" : "nav-item-unloaded"
+
         return (
-            <p className="gradient-hover nav-item" onClick={this.onClick}>{this.props.text}</p>
+            <p className={"gradient-hover nav-item " + className} onClick={this.onClick}>{this.props.text}</p>
         );
     }
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,11 +3,30 @@ import NavItem from './NavItem';
 
 class Navbar extends Component {
     render() {
+        let items = [{
+            text: "About Me",
+            section: "aboutMe"
+        }, {
+            text: "Experience",
+            section: "experience"
+        }, {
+            text: "Projects",
+            section: "projects"
+        }]
+        let navItems = []
+
+        for (let i = 0; i < items.length; i++) {
+            let current = items[i]
+            let text = current.text
+            let section = current.section
+            let order = (items.length - 1) - i
+            
+            navItems.push(<NavItem text={text} section={section} order={order} key={i}/>)
+        }
+
         return (
             <div className="nav-header">
-                <NavItem text={"About Me"} section={"aboutMe"} />
-                <NavItem text={"Experience"} section={"experience"} />
-                <NavItem text={"Projects"} section={"projects"} />
+            {navItems}
             </div>
         );
     }
