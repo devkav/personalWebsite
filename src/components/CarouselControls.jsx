2d1aace817c6800acf0b3f1d09fbf1b32c750c26
Devan Kavalchek <devankav7@gmail.com>
2022-01-20 14:21:25 -0500

    hid arrows at ends

--- src/components/CarouselControls.jsx
+++ src/components/CarouselControls.jsx
@@ -10,12 +10,17 @@ class CarouselControls extends Component {
     render() {
         let numBubbles = this.props.numBubbles
         let active = this.props.active
+        let beforeButtonClassNames = "carousel-btn clickable"
+        let afterButtonClassNames = "carousel-btn clickable"
+
+        if (this.props.active == 0) {beforeButtonClassNames += " carousel-btn-inactive"}
+        if (this.props.active == numBubbles - 1) {afterButtonClassNames += " carousel-btn-inactive"}
 
         return (
             <div className="carousel-controls">
-                <MdNavigateBefore onClick={this.props.previousCallback} className="carousel-btn clickable" />
+                <MdNavigateBefore onClick={this.props.previousCallback} className={beforeButtonClassNames} />
                 <CarouselIndicator numBubbles={numBubbles} active={active} setActive={this.setActive}/>
-                <MdNavigateNext onClick={this.props.nextCallback} className="carousel-btn clickable" />
+                <MdNavigateNext onClick={this.props.nextCallback} className={afterButtonClassNames} />
             </div>
         );
     }
