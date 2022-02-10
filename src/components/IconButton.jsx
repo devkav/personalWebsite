0b21c375589004bc752293e4c934137c6cc0c268
Devan Kavalchek <devankav7@gmail.com>
2022-02-09 22:07:08 -0500

    Updates

--- src/components/IconButton.jsx
+++
@@ -1,9 +0,0 @@
-function IconButton(props) {
-    return (
-        <a href={props.href} className="icon-btn" target="_blank" rel="noreferrer">
-            {props.children}
-        </a>
-    );
-}
-
-export default IconButton;
