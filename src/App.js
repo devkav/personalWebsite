f4edbaf5bc2e273f1030aa3557b34f8d8fe1c0d5
Devan Kavalchek <devankav7@gmail.com>
2023-04-17 14:24:51 -0400

    Bump dependencies and add typescript

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import './style/App.css';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import SiteContent from './pages/SiteContent';
-import { Fragment } from 'react/cjs/react.production.min';
-import MetaTags from 'react-meta-tags';
+import {Fragment} from 'react/cjs/react.production.min';
+import {Helmet} from 'react-helmet';
 import PageNotFound from './pages/PageNotFound';
 
 const darkModeBgColor = 'rgb(30, 30, 30)';
@@ -11,42 +11,47 @@ const darkModeCardColor = 'rgb(40, 40, 40)';
 
 const lightModeBgColor = '#fff';
 const lightModeTextColor = '#212529';
-const lightModeCardColor = '#fff'
+const lightModeCardColor = '#fff';
 
 function App(props) {
-    const toggleDarkMode = (enabled) => {
-        document.documentElement.style.setProperty(
-            '--bs-body-color', enabled ? darkModeTextColor : lightModeTextColor
-        );
-        document.documentElement.style.setProperty(
-            '--bs-body-bg', enabled ? darkModeBgColor : lightModeBgColor
-        );
-        document.documentElement.style.setProperty(
-            '--bs-card-color', enabled ? darkModeCardColor : lightModeCardColor
-        );
-    }
+  const toggleDarkMode = (enabled) => {
+    document.documentElement.style.setProperty(
+      '--bs-body-color',
+      enabled ? darkModeTextColor : lightModeTextColor
+    );
+    document.documentElement.style.setProperty(
+      '--bs-body-bg',
+      enabled ? darkModeBgColor : lightModeBgColor
+    );
+    document.documentElement.style.setProperty(
+      '--bs-card-color',
+      enabled ? darkModeCardColor : lightModeCardColor
+    );
+  };
 
-    const getThemeColor = () => {
-        const prefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
-        toggleDarkMode(prefersDark)
-        let themeColor = prefersDark ? darkModeBgColor : lightModeBgColor;
+  const getThemeColor = () => {
+    const prefersDark = window.matchMedia(
+      '(prefers-color-scheme: dark)'
+    ).matches;
+    toggleDarkMode(prefersDark);
+    let themeColor = prefersDark ? darkModeBgColor : lightModeBgColor;
 
-        return themeColor
-    }
+    return themeColor;
+  };
 
-    return (
-        <Fragment>
-            <MetaTags>
-                <meta name="theme-color" content={getThemeColor()} />
-            </MetaTags>
-            <Router>
-                <Routes>
-                    <Route path="/" element={<SiteContent />} exact />
-                    <Route path="*" element={<PageNotFound />} />
-                </Routes>
-            </Router>
-        </Fragment>
-    )
+  return (
+    <Fragment>
+      <Helmet>
+        <meta name='theme-color' content={getThemeColor()} />
+      </Helmet>
+      <Router>
+        <Routes>
+          <Route path='/' element={<SiteContent />} exact />
+          <Route path='*' element={<PageNotFound />} />
+        </Routes>
+      </Router>
+    </Fragment>
+  );
 }
 
 export default App;
